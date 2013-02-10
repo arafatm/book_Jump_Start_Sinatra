cdd78b8da7656ac192bd25029241c1362aaca0e3
Arafat Mohamed <github@arafatm.com>
2013-02-10 14:45:16 -0600

    named variables

--- ch01/hello.rb
+++ ch01/hello.rb
@@ -7,3 +7,14 @@ Bundler.require
 get '/hello' do
     "Hello Sinatra!"
 end
+
+get '/:name' do
+    name = params[:name]
+      "Hi there #{name}!"
+end
+
+get '/what/time/is/it/in/:number/hours' do
+  number = params[:number].to_i
+  time = Time.now + number * 3600
+  "The time in #{number} hours will be #{time.strftime('%I:%M %p')}"
+end
