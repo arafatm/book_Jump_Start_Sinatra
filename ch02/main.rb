ed03d2a47869ae839a742af60ee054d967beab07
Arafat Mohamed <github@arafatm.com>
2013-02-10 15:21:04 -0600

    more example views with layout

--- ch02/main.rb
+++ ch02/main.rb
@@ -4,6 +4,14 @@ get '/' do
   erb :home
 end
 
+get '/about' do
+    erb :about
+end
+
+get '/contact' do
+    erb :contact
+end
+
 __END__
 @@layout
 <% title="Songs By Sinatra" %>
@@ -31,5 +39,11 @@ __END__
 </html>
 
 @@home
-<p>Welcome to this website that's all about the songs of the great
-Frank Sinatra.</p>
+<p>Welcome to this website that's all about the songs of the great Frank
+Sinatra.</p>
+
+@@about
+<p>This site is a demonstration of how to build a website using Sinatra.</p>
+
+@@contact
+<p>You can contact me by sending an email to daz at gmail.com</p>
